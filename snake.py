ce53349ece7647a174d5f0c0023a26d3dab1e977
tfhappy <you@example.com>
2019-10-19 21:24:19 +0800

    beyond the boundary

--- snake.py
+++ snake.py
@@ -1,9 +1,12 @@
 # -*- coding:utf-8 -*-
 import sys
+import time
+
 import pygame
 import random
 
 pygame.init()  # 初始化所有模块
+pygame.display.set_caption('贪吃蛇')  # 设置窗口标题
 size = width, height = 800, 600
 
 screen = pygame.display.set_mode(size)  # 创建一个窗口
@@ -31,6 +34,28 @@ move_up = (0, -speed)  # 越往上, top值越小
 move_down = (0, speed)
 # 移动方向
 move_to = move_right  # 初始方向为向右
+
+
+def game_over(screen):
+    # pygame.font.Font: 创建一个字体对象
+    font = pygame.font.Font(None, 72)
+    # pygame.font.Font.render: 画字到一个suface
+    font_surface = font.render("GAME OVER!", True, (255, 255, 0), black)
+    # pygame.Surface.get_size: 获取suface的尺寸
+    font_rect = font_surface.get_rect()
+    # 把窗口的中心位置 赋值给suface的中心位置
+    font_rect.center = screen.get_rect().center
+    # blit: 把suface画到窗口
+    screen_rect = screen.blit(font_surface, font_rect)
+    # update: 局部更新窗口
+    pygame.display.update(screen_rect)
+    # 等待游戏退出
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: sys.exit()
+        time.sleep(0.1)
+
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
@@ -43,6 +68,11 @@ while True:
                 move_to = move_left
             if event.key == pygame.K_RIGHT and move_to != move_left:
                 move_to = move_right
+    # 越界退出游戏
+    if snake_head.left < 0 or snake_head.right > width:
+        game_over(screen)
+    if snake_head.top < 0 or snake_head.bottom > height:
+        game_over(screen)
 
     screen.fill(black)  # 填充黑色背景
 
